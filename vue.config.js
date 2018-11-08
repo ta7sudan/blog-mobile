ac1e0a08cbdc148addf02daaba30a416bb027d26
ta7sudan <ta7sudan@gmail.com>
2018-11-08 14:52:52 +0800

    add publichPath, css sourcemap and mainFields

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  lintOnSave: false
+  baseUrl: process.env.PUBLIC_PATH,
+  css: {
+    sourceMap: true
+  },
+  configureWebpack: {
+    resolve: {
+      mainFields: ["module", "browser", "main"]
+    }
+  }
 };
