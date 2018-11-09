3f84b93018addc6ea86d4e9d6aea2c0c488fb990
ta7sudan <ta7sudan@gmail.com>
2018-11-09 12:05:00 +0800

    add postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,40 @@
 module.exports = {
 	plugins: {
-		autoprefixer: {}
+		autoprefixer: {},
+		// mixins要放precss前面
+		'postcss-mixins': {
+			mixins: {}
+		},
+		precss: {},
+		'postcss-hexrgba': {},
+		'postcss-sprites': {
+			filterBy(img) {
+				if (img.url.includes('nosprites')) {
+					return Promise.reject();
+				}
+				return Promise.resolve();
+			// },
+			// TODO
+			// groupBy(img) {
+			}
+		},
+		'postcss-functions': {
+			functions: {
+				test(val) {
+					return val + 'px';
+				}
+			}
+		},
+		// 这个放最后, 不然可能functions里有px而不会被转换,
+		// 或是sprites有px也不会被转换
+		'postcss-px-to-viewport': {
+			unitToConvert: 'px',
+			viewportWidth: 750,
+			unitPrecision: 3,
+			viewportUnit: 'vw',
+			fontViewportUnit: 'px',
+			minPixelValue: 1,
+			mediaQuery: false
+		}
 	}
 };
