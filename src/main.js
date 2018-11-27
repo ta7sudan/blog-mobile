19856d2f2e2903c0820c887ba88320faad6585b3
ta7sudan <ta7sudan@gmail.com>
2018-11-27 22:23:13 +0800

    update

--- src/main.js
+++ src/main.js
@@ -12,14 +12,10 @@ if (DEBUG) {
 }
 
 Vue.config.productionTip = false;
-// 理论上讲, apis可以做成插件, 对于在Vue中的使用来说更为合理一些
-// 不过考虑到发送请求这一需求不仅仅是在Vue的生命周期中存在,
-// 在其他地方也会有, 所以还是将apis暴露在外面比较好,
-// 也许之后想到什么更为优雅的方案再改吧
-Vue.prototype.$apis = apis;
 
 new Vue({
 	router,
+	apis,
 	store,
 	render: h => h(App)
 }).$mount('#app');
